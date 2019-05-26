ce2e532fd52378110530956399d0ff4fb9eff68f
Anya Efimova <gani4ka_n@ukr.net>
2019-05-26 21:18:42 +0300

    Updates

---
+++ precache-manifest.e565251278ad059acddab8db1f8d48ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732f049ed79e221c7d7f0da643fd548d",
+    "url": "/React-HW/index.html"
+  },
+  {
+    "revision": "16283e6aaeaf961b648c",
+    "url": "/React-HW/static/css/main.192c8f2d.chunk.css"
+  },
+  {
+    "revision": "294a3c23aa9869ae810b",
+    "url": "/React-HW/static/js/2.7272bfd8.chunk.js"
+  },
+  {
+    "revision": "16283e6aaeaf961b648c",
+    "url": "/React-HW/static/js/main.6e361b9d.chunk.js"
+  },
+  {
+    "revision": "62f1aaa7e315adccc8e3",
+    "url": "/React-HW/static/js/runtime~main.236b5772.js"
+  }
+]);
